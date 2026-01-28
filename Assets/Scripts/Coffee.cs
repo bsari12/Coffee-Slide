6e556499dcc90b8a44c6a1b9933b3ea0d26a14bc
bsari12 <berkaysari1225@gmail.com>
2026-01-28 15:43:54 +0300

    Merge Physics

--- Assets/Scripts/Coffee.cs
+++ Assets/Scripts/Coffee.cs
@@ -3,30 +3,23 @@ using UnityEngine;
 public class Coffee : MonoBehaviour
 {
     [Header("Identity")]
-    public int coffeeLevelIndex; // 0=C1, 1=C2, 2=C3...
+    public int coffeeLevelIndex; 
     
-    private bool hasMerged = false; // Aynı anda iki kez merge olmasın diye kilit
-
+    private bool hasMerged = false; 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         float impactVelocity = collision.relativeVelocity.magnitude;
         AudioManager.Instance.PlayCollision(impactVelocity);
-        
-        // Eğer zaten birleşme sürecindeysem işlem yapma
+
         if (hasMerged) return;
 
-        // Çarptığım şeyin üzerinde "Coffee" scripti var mı?
         Coffee otherCoffee = collision.gameObject.GetComponent<Coffee>();
 
         
         if (otherCoffee != null)
         {
-            // Seviyelerimiz aynı mı? (C1 vs C1 mi?)
             if (otherCoffee.coffeeLevelIndex == this.coffeeLevelIndex)
             {
-                // BURASI ÖNEMLİ: İki kahve çarpışınca kod iki kez çalışır (biri ben, biri o).
-                // Sadece biri işlemi yapsın diye InstanceID kontrolü yapıyoruz.
-                // Kimliği büyük olan işlemi üstlensin.
                 if (this.gameObject.GetInstanceID() > otherCoffee.gameObject.GetInstanceID())
                 {
                     MergeWith(otherCoffee);
@@ -42,18 +35,13 @@ public class Coffee : MonoBehaviour
 
         Vector3 middlePos = (this.transform.position + other.transform.position) / 2f;
 
-        // --- YENİ KISIM: HIZ HESAPLAMA ---
-        // Benim hızım ve çarptığımın hızını al
         Rigidbody2D myRb = this.GetComponent<Rigidbody2D>();
         Rigidbody2D otherRb = other.GetComponent<Rigidbody2D>();
 
-        // İki vektörü toplayıp 2'ye bölerek ortalama hızı buluyoruz.
-        // Böylece çarpışma yönüne doğru akmaya devam ederler.
         Vector2 averageVelocity = (myRb.linearVelocity + otherRb.linearVelocity) / 2f;
         
         AudioManager.Instance.PlayMerge();
 
-        // Hesapladığımız hızı Manager'a gönderiyoruz
         CoffeeServer.Instance.SpawnNextLevelCoffee(this.coffeeLevelIndex, middlePos, averageVelocity);
 
         Destroy(other.gameObject);
