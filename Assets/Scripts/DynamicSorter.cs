74ed84612b96a00856f6a50770a7bc5f732bb926
bsari12 <berkaysari1225@gmail.com>
2026-01-28 22:23:50 +0300

    Sprite Sorterer

--- Assets/Scripts/DynamicSorter.cs
+++ Assets/Scripts/DynamicSorter.cs
@@ -3,19 +3,24 @@ using UnityEngine;
 public class DynamicSorter : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
-    
+    private Rigidbody2D rb; 
     private const int SORTING_PRECISION = 100; 
-    
     public int sortingOffset = 0;
 
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     void LateUpdate()
     {
-        
-        spriteRenderer.sortingOrder = (int)(transform.position.y * -SORTING_PRECISION) + sortingOffset;
+        if (rb != null && rb.IsSleeping()) return;
+
+        if (transform.hasChanged)
+        {
+            spriteRenderer.sortingOrder = (int)(transform.position.y * -SORTING_PRECISION) + sortingOffset;
+            transform.hasChanged = false; 
+        }
     }
 }
